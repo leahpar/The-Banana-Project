e06a5912eca5e4e469c0c0a51ecc3756e485e405
Raphaël Bacco <raphael@bacco.fr>
2023-06-03 19:33:22 +0200

    apache pack !

--- src/Controller/PushController.php
+++ src/Controller/PushController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Minishlink\WebPush\VAPID;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
