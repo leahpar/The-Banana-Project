36e296fb7ff451e48b302b33b611f13f9420024e
Raphaël Bacco <raphael@bacco.fr>
2023-06-07 11:04:56 +0200

    Update PushCommand.php

--- src/Command/PushCommand.php
+++ src/Command/PushCommand.php
@@ -35,6 +35,8 @@ class PushCommand extends Command
     {
         $this
             ->addArgument('message', InputArgument::OPTIONAL, 'Message to send (default: "Hello world!")')
+            // option to send to a specific user
+            ->addOption('user', 'u', InputOption::VALUE_REQUIRED, 'User ID to send the message to')
         ;
     }
 
@@ -43,12 +45,23 @@ class PushCommand extends Command
         $io = new SymfonyStyle($input, $output);
 
         $message = $input->getArgument('message') ?? 'Hello world!';
+        $userId = $input->getOption('user');
 
-        $users = $this->em->getRepository(User::class)->findAll();
+        if ($userId) {
+            $user = $this->em->getRepository(User::class)->find($userId);
+            if ($user === null) {
+                $io->error("User not found");
+                return Command::FAILURE;
+            }
+            $users = [$user];
+        }
+        else {
+            $users = $this->em->getRepository(User::class)->findAll();
+        }
 
         $webPush = new WebPush([
             'VAPID' => [
-                'subject' => 'webpush.local',
+                'subject' => 'bananaproject.inara.ovh',
                 'publicKey' => $this->vapidPublicKey,
                 'privateKey' => $this->vapidPrivateKey,
             ]
@@ -56,27 +69,52 @@ class PushCommand extends Command
 
         /** @var User $user */
         foreach ($users as $user) {
-            $webPush->queueNotification(
+            $report = $webPush->sendOneNotification(
                 Subscription::create($user->getSuscription()),
                 json_encode([
                     'title' => 'The Banana Project',
                     'message' => $message,
                 ]),
             );
-        }
-
-        foreach ($webPush->flush() as $report) {
             $endpoint = $report->getRequest()->getUri()->__toString();
-            // truncate $endpoint
             $endpoint = substr($endpoint, 0, 80) . '...';
-
             if ($report->isSuccess()) {
-                $io->writeln("Message sent successfully for <comment>{$endpoint}</comment>");
-            } else {
-                $io->writeln("<error>KO</error> Message failed to sent for subscription {$endpoint}: {$report->getReason()}");
+                $io->writeln("[{$user->id}] Message sent successfully for <comment>{$endpoint}</comment>");
+            }
+            elseif ($report->isSubscriptionExpired()) {
+                // 404 or 410, unsubscribe
+                $io->writeln("[{$user->id}] <error>KO</error> Subscription gone {$endpoint}: {$report->getReason()}");
+                $this->em->remove($user);
+            }
+            else {
+                $io->writeln("[{$user->id}] <error>KO</error> Message failed {$endpoint}: {$report->getReason()}");
             }
         }
 
+//        /** @var User $user */
+//        foreach ($users as $user) {
+//            $webPush->queueNotification(
+//                Subscription::create($user->getSuscription()),
+//                json_encode([
+//                    'title' => 'The Banana Project',
+//                    'message' => $message,
+//                ]),
+//            );
+//        }
+//
+//        foreach ($webPush->flush() as $report) {
+//            $endpoint = $report->getRequest()->getUri()->__toString();
+//            // truncate $endpoint
+//            $endpoint = substr($endpoint, 0, 80) . '...';
+//
+//            if ($report->isSuccess()) {
+//                $io->writeln("[] Message sent successfully for <comment>{$endpoint}</comment>");
+//            } else {
+//                $io->writeln("<error>KO</error> Message failed to sent for subscription {$endpoint}: {$report->getReason()}");
+//            }
+//        }
+
+        $this->em->flush();
         $io->success('OK');
 
         return Command::SUCCESS;
